e282f731b97cb942ff61f08c53e6e38cf074b6ee
anastasia.kuperberg <anastasia.kuperberg@cyberfox.cz>
2025-10-23 11:52:22 +0200

    tailwind and navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TranslatePipe, TranslateDirective} from '@ngx-translate/core';
+import { Nav } from "./components/nav/nav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslatePipe, TranslateDirective],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
