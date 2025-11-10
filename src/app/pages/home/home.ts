3c529d2f758d078e20c38bf6e7181854c3a2eb87
anastasia.kuperberg <anastasia.kuperberg@cyberfox.cz>
2025-11-10 14:25:03 +0100

    ui

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatRadioModule } from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
+import { SelectedOption } from './consts/consts';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule, MatButtonModule, MatInputModule, MatFormField, MatLabel, MatIconModule, MatRadioModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  selectedOption: SelectedOption = 'recommend';
 }
